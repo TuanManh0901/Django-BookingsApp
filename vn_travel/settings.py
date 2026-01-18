b84736fc312a9b4d9de692860b874e8daf893f40
TuanManh0901 <dinhtuanmanh0901@gmail.com>
2026-01-18 23:28:16 +0700

    fix: Add STATIC_ROOT for Render production
    Required for collectstatic to work in deployment

--- vn_travel/settings.py
+++ vn_travel/settings.py
@@ -185,6 +185,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # For production collectstatic
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
