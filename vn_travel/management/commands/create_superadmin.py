11b16a267dc815c43cbe59fb4c054c842db9644a
TuanManh0901 <dinhtuanmanh0901@gmail.com>
2026-01-20 15:56:42 +0700

    fix: Remove admin rights from manh0 user on deploy

--- vn_travel/management/commands/create_superadmin.py
+++ vn_travel/management/commands/create_superadmin.py
@@ -25,3 +25,14 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f'Created superuser: {username}'))
         
         self.stdout.write(self.style.SUCCESS(f'Login: {username} / {password}'))
+        
+        # Remove admin rights from manh0
+        try:
+            manh0_user = User.objects.get(username='manh0')
+            manh0_user.is_staff = False
+            manh0_user.is_superuser = False
+            manh0_user.save()
+            self.stdout.write(self.style.SUCCESS('Removed admin rights from manh0'))
+        except User.DoesNotExist:
+            self.stdout.write('manh0 user not found, skipping')
+
